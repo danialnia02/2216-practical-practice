57a3200208f8acdd9c2a6dd129eb97c9cc6a2cc8
Mohamed Danial Irfan Bin Salimi <2400797@sit.singaporetech.edu.sg>
2026-07-23 01:51:28 +0800

    made some changes

--- web/server.js
+++ web/server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const routes = require("./routes");
 
 const app = express();
+app.disable("x-powered-by");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: false }));
